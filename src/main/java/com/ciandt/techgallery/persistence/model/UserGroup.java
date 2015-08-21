6a7f4a789fddefd51e94c9607fe7586b3a7c079c
felipers <felipers@cit0012809.cit>
2015-08-21 14:58:30 -0300

    DAO start implementation. Also applied google checkstyle to previous files.

---
+++ src/main/java/com/ciandt/techgallery/persistence/model/UserGroup.java
@@ -0,0 +1,38 @@
+package com.ciandt.techgallery.persistence.model;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+/**
+ * Entity class for a User Group.
+ * 
+ * @author felipers
+ *
+ */
+@Entity
+public class UserGroup {
+
+  @Id
+  private Long id;
+  /** user's group name. */
+  @Index
+  private String name;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
