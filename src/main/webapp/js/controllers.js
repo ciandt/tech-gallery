3039eee58ff19a2d14a91cadb71044ccda554024
Thulio Ribeiro <tribeiro@ciandt.com>
2015-08-24 10:56:10 -0300

    Montagem da estrutura de Frontend.

---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var techListController = [
+		'$scope',
+		'$http',
+		'$location',
+		'$routeParams',
+		'$route',
+		function($scope, $http, $location, $routeParams) {
+
+			$scope.techList = getTechList();
+
+			$scope.populateList = function() {
+				$scope.techList = getTechList();
+			}
+
+			$scope.showList = function() {
+				$scope.showListTech = true;
+			};
+
+			$scope.hideList = function() {
+				$scope.showListTech = false;
+			};
+
+			$scope.addTech = function() {
+				var tech = {};
+				tech.name = $scope.techName;
+				tech.desc = $scope.techDesc;
+				$scope.techList.push(tech);
+				cleanList();
+			}
+
+			function cleanList() {
+				$scope.techName = "";
+				$scope.techDesc = "";
+			}
+
+			function getTechList() {
+				var descr = "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis.";
+				var list = [ {
+					name : "Angular",
+					desc : descr,
+					image : "/image/ANGULAR.png"
+				}, {
+					name : "Google App Engine",
+					desc : descr,
+					image : "/image/GAE.png"
+				}, {
+					name : "Google Compute Engine",
+					desc : descr,
+					image : "/image/GCE.png"
+				}, {
+					name : "Google Cloud Storage",
+					desc : descr,
+					image : "/image/GCS.png"
+				}, {
+					name : "Google Big Query",
+					desc : descr,
+					image : "/image/BQ.png"
+				}, {
+					name : "BootStrap",
+					desc : descr,
+					image : "/image/BOOT.png"
+				} ];
+				return list;
+			}
+		} ];
