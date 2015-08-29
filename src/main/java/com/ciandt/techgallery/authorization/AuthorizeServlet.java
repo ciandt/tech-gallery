bac46d099eefb7816d175b14949bde9e75ca399f
edugfilho <edugomfilho@gmail.com>
2015-08-29 00:08:23 -0300

    TECG-40 Authorization working. Need to make it required for all users, everywhere across the system.

--- src/main/java/com/ciandt/techgallery/authorization/AuthorizeServlet.java
+++ src/main/java/com/ciandt/techgallery/authorization/AuthorizeServlet.java
@@ -1,33 +1,29 @@
 package com.ciandt.techgallery.authorization;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.extensions.appengine.auth.oauth2.AbstractAppEngineAuthorizationCodeServlet;
 
 public class AuthorizeServlet extends AbstractAppEngineAuthorizationCodeServlet {
 
-    private static final long serialVersionUID = 1L;
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws IOException {
-      // do stuff
-    }
-    
-    @Override
-    protected String getRedirectUri(HttpServletRequest req)
-            throws ServletException, IOException {
-        return OAuthUtils.getRedirectUri(req);
-    }
+  private static final long serialVersionUID = 1L;
 
-    @Override
-    protected AuthorizationCodeFlow initializeFlow() throws IOException {
-        return OAuthUtils.newFlow();
-    }
+  /**
+   * Is called by Google after the authorization process, with the authorization 
+   * token incorporated into the request. The URI must be registered
+   * on the developer's console and also be in the client secrets file. This protects
+   * against another app with a stolen client secret from stealing end-user's auth token.
+   */
+  @Override
+  protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
+    return OAuthUtils.getRedirectUri(req);
+  }
+
+  @Override
+  protected AuthorizationCodeFlow initializeFlow() throws IOException {
+    return OAuthUtils.newFlow();
+  }
 
 }
