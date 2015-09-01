2bb950a220b92e80dbca9286e9f8e1b73ca103f1
edugfilho <edugomfilho@gmail.com>
2015-09-01 14:13:22 -0300

    Adding missing comment

--- src/main/java/com/ciandt/techgallery/authorization/OAuthUtils.java
+++ src/main/java/com/ciandt/techgallery/authorization/OAuthUtils.java
@@ -74,7 +74,10 @@ public class OAuthUtils {
   }
 
   /**
-   * TODO comment this
+   * Can be used to manually refresh the access token.
+   * When using App Engine libraries this is not necessary as
+   * they refresh the access token automatically when they are
+   * about to expire
    * 
    * @param user
    * @throws IOException
