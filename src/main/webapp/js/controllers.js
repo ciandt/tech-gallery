e28b7460221d0735a3f1d1e8906e47c1ecf410a6
Thulio Ribeiro <tsr_thulio@hotmail.com>
2015-09-02 09:46:37 -0300

    Front-end adjust

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -143,7 +143,7 @@ angular
                     function mockTechnology() {
                         $scope.name = "Nome da tecnologia de id ";
                         $scope.description = "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.";
-                        $scope.enterpriseRecommendation = "Digite aqui a recomendação da sua empresa para esta tecnologia";
+                        $scope.recommendation = "Digite aqui a recomendação da sua empresa para esta tecnologia";
                         $scope.image = "https://storage.googleapis.com/tech-gallery-assets/imagesLogo/adf.png";
                     }
 
@@ -156,7 +156,7 @@ angular
                                 function(data) {
                                     $scope.name = data.name;
                                     $scope.description = data.description;
-                                    $scope.enterpriseRecommendation = data.enterpriseRecommendation;
+                                    $scope.recommendation = data.recommendation;
                                     $scope.image = data.image;
                                     $scope.$apply();
                                 });
