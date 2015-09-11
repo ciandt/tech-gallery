00b765d763f7071caf004c148d34a3267231d204
Gabriel Izaias <gabriel@izaias.co>
2015-09-11 16:11:03 -0300

    [TECG-125] Add csslint task and switch to checkstyle reporter

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -2,9 +2,18 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var csslint = require('gulp-csslint');
 
-gulp.task('lint', function() {
+gulp.task('lint', ['jshint', 'csslint']);
+
+gulp.task('jshint', function() {
   return gulp.src('./js/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('checkstyle'));
+});
+
+gulp.task('csslint', function() {
+  return gulp.src('./css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.reporter('checkstyle-xml'));
 });
