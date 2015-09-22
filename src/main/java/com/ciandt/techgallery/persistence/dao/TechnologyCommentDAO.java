3e0e612e5ac6b0db44ffdd6e2c789e60769fe654
João Felipe de Medeiros Moreira <joaom@ciandt.com>
2015-09-22 09:54:45 -0300

    TECG-25/TECG-135-Entity and DAO

---
+++ src/main/java/com/ciandt/techgallery/persistence/dao/TechnologyCommentDAO.java
@@ -0,0 +1,14 @@
+package com.ciandt.techgallery.persistence.dao;
+
+import com.ciandt.techgallery.persistence.model.TechnologyComment;
+
+/**
+ * Interface of TechnologyComment
+ *
+ * @author <a href="mailto:joaom@ciandt.com"> João Felipe de Medeiros Moreira </a>
+ * @since 22/09/2015
+ *
+ */
+public interface TechnologyCommentDAO extends GenericDAO<TechnologyComment, Long> {
+
+}
---
+++ src/main/java/com/ciandt/techgallery/persistence/dao/TechnologyCommentDAOImpl.java
@@ -0,0 +1,15 @@
+package com.ciandt.techgallery.persistence.dao;
+
+import com.ciandt.techgallery.persistence.model.TechnologyComment;
+
+/**
+ * Class that implements DAO of TechnologyComment
+ *
+ * @author <a href="mailto:joaom@ciandt.com"> João Felipe de Medeiros Moreira </a>
+ * @since 22/09/2015
+ *
+ */
+public class TechnologyCommentDAOImpl extends GenericDAOImpl<TechnologyComment, Long>
+    implements TechnologyCommentDAO {
+
+}
