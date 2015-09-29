8960bc897e5079861df6fa447818cdce843e5674
Eduardo Gomes Filho <edugomfilho@gmail.com>
2015-09-29 18:25:06 -0300

    [TECG-92]/[TECG-171]: Detecting browser language

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -1,8 +1,16 @@
 angular.module('techGallery').config([ '$translateProvider', function($translateProvider) {
-	$translateProvider.translations('en', {
+	$translateProvider.translations('en-US', {
+		'TITLE' : 'Tech Gallery ENG',
+		'FOO' : 'This is a paragraph'
+	});
+	$translateProvider.translations('pt-BR', {
 		'TITLE' : 'Tech Gallery',
 		'FOO' : 'This is a paragraph'
 	});
-
-	$translateProvider.preferredLanguage('en');
+	var lang = navigator.language;
+	if(lang != 'pt-BR' && lang != 'en-US') {
+		lang = 'en-US';
+	} 
+	$translateProvider.preferredLanguage(lang);
+	
 } ]);
