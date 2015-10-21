c36a01e981acb582469bdd38c91462d499d7823c
Felipe Ibrahim <felipeaziz@gmail.com>
2015-10-21 17:02:50 -0200

    [TECG-296]/[TECG-327]-Front-end implementations

---
+++ src/main/java/com/ciandt/techgallery/persistence/dao/TechnologyFollowersDAO.java
@@ -0,0 +1,20 @@
+package com.ciandt.techgallery.persistence.dao;
+
+import com.ciandt.techgallery.persistence.model.Technology;
+import com.ciandt.techgallery.persistence.model.TechnologyFollowers;
+
+/**
+ * TechnologyFollowersDAOImpl methods interface.
+ *
+ * @author ibrahim
+ *
+ */
+public interface TechnologyFollowersDAO extends GenericDAO<TechnologyFollowers, Long> {
+
+  /**
+   * Method to find TechnologyFollowers by Technology
+   * @param technology technology info.
+   * @return TechnologyFollowers or null;
+   */
+  public TechnologyFollowers findByTechnology(Technology technology);
+}
