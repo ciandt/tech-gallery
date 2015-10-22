f8c516e70ca4d443c17d7560586e8bcc59918db1
brunoliberal <liberalaraujo@gmail.com>
2015-10-22 14:28:29 -0200

    [TECG-296][TECG-339] Refactoring service.

--- src/main/java/com/ciandt/techgallery/service/EmailService.java
+++ src/main/java/com/ciandt/techgallery/service/EmailService.java
@@ -2,16 +2,14 @@ package com.ciandt.techgallery.service;
 
 import com.ciandt.techgallery.persistence.model.TechGalleryUser;
 import com.ciandt.techgallery.persistence.model.Technology;
-import com.ciandt.techgallery.persistence.model.TechnologyComment;
-import com.ciandt.techgallery.persistence.model.TechnologyRecommendation;
-
-import java.util.List;
 
 
 public interface EmailService {
-  void push(TechGalleryUser user, Technology technology,
-      List<TechnologyRecommendation> recommendations, List<TechnologyComment> comments);
+  void push(TechGalleryUser user, Technology technology, String recommendationsIds,
+      String commentsIds);
 
   void execute(String userId, String technologyId, String recommendationsIds, String commentsIds,
       String serverUrl);
+  
+  void sendDailyEmailtoFollowers();
 }
