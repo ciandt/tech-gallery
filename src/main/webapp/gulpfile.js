a5b6eb0633f20b4d201dcac89f8e1c8fc3b44008
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-10-22 18:23:48 -0200

    [TECG-272][TECG-318] Add sass gulp task
    Transform current stylesheets into Sass files

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -4,12 +4,45 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var csslint = require('gulp-csslint');
+var gutil = require('gulp-util');
 var del = require('del');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+/**
+ * Files path
+ * @type {Object}
+ */
+var src = {
+  scripts: {
+    all: 'app/**/*.js',
+    app: 'app/app.js'
+  },
+  styles: {
+    all: 'assets/stylesheets/**/*.scss',
+    app: 'assets/stylesheets/app.scss'
+  }
+};
+
 /**
  * Build folder prefix
  * @type {String}
  */
 var build = 'build/';
+
+/**
+ * Output folder
+ * @type {Object}
+ */
+var out = {
+  scripts: {
+    file: 'app.min.js',
+    folder: build
+  },
+  styles: {
+    file: 'app.min.css',
+    folder: build
+  }
+};
 
 
 gulp.task('lint', ['jshint', 'csslint']);
@@ -18,6 +51,14 @@ gulp.task('clean', function () {
   return del([
     build
   ]);
+});
+
+gulp.task('sass', function () {
+  gulp.src(src.styles.app)
+    .pipe(sass().on('error', gutil.log))
+    // TODO: minify stylesheets
+    .pipe(rename(out.styles.file))
+    .pipe(gulp.dest(out.styles.folder));
 });
 
 gulp.task('jshint', function() {
