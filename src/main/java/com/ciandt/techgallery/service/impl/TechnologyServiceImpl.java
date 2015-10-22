f9b9c7b82c5c0a75916cc5904c5b42035d17ef50
Felipe Ibrahim <felipeaziz@gmail.com>
2015-10-22 14:35:11 -0200

    [TECG-215]/[TECG-365]-import-user-skill: bug fix

--- src/main/java/com/ciandt/techgallery/service/impl/TechnologyServiceImpl.java
+++ src/main/java/com/ciandt/techgallery/service/impl/TechnologyServiceImpl.java
@@ -79,7 +79,7 @@ public class TechnologyServiceImpl implements TechnologyService {
 
     validateInformations(technology);
     String imageLink = technology.getImage();
-    if (technology.getRecommendation() == null) {
+    if (technology.getRecommendation() == null && technology.getImage() != null) {
       imageLink = storageDAO.insertImage(convertNameToId(technology.getName()),
           new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(technology.getImage())));
     }
