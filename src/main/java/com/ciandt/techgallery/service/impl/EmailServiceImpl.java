a8710e3ebcea926f605105c22e928253bdde56cd
Felipe Ibrahim <felipeaziz@gmail.com>
2015-10-23 14:41:42 -0200

    [TECG-296]-follow-technology: Email service

--- src/main/java/com/ciandt/techgallery/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ciandt/techgallery/service/impl/EmailServiceImpl.java
@@ -58,16 +58,16 @@ public class EmailServiceImpl implements EmailService {
    * Attributes --------------------------------------------
    */
   private static EmailServiceImpl instance;
-  
+
   private static final Logger log = Logger.getLogger(EmailServiceImpl.class.getName());
   private static final String queueName = "email-queue";
   private static final String queueUrl = "/mail";
 
-  //TODO: move inside create template.
+  // TODO: move inside create template.
   private static final String template = "template.example.email";
   private static final String subject = "[Tech Gallery] Resumo do dia";
   private static final String reason = "Resumo do dia para os followers";
-  
+
   /*
    * Constructors --------------------------------------------
    */
@@ -87,36 +87,35 @@ public class EmailServiceImpl implements EmailService {
     }
     return instance;
   }
-  
+
   private InternetAddress from = null;
   private EmailNotificationDAO emailNotificationDao = EmailNotificationDAOImpl.getInstance();
   private CronJobDAO cronJobsDao = CronJobDAOImpl.getInstance();
   private TechnologyFollowersDAO technologyFollowersDao = TechnologyFollowersDAOImpl.getInstance();
-  private TechnologyRecommendationDAO technologyRecommendationDao = TechnologyRecommendationDAOImpl
-      .getInstance();
+  private TechnologyRecommendationDAO technologyRecommendationDao =
+      TechnologyRecommendationDAOImpl.getInstance();
   private TechnologyCommentDAO technologyCommentDao = TechnologyCommentDAOImpl.getInstance();
-  
+
   @Override
   public void push(TechGalleryUser user, Technology technology, String recommendationsIds,
       String commentsIds) {
-    QueueFactory.getQueue(queueName).add(TaskOptions.Builder.withUrl(queueUrl)
-        .param("userId", user.getId().toString())
-        .param("technologyId", technology.getId())
-        .param("recommendationsIds", recommendationsIds)
-        .param("commentsIds", commentsIds));
+    QueueFactory.getQueue(queueName)
+        .add(TaskOptions.Builder.withUrl(queueUrl).param("userId", user.getId().toString())
+            .param("technologyId", technology.getId())
+            .param("recommendationsIds", recommendationsIds).param("commentsIds", commentsIds));
   }
 
   @Override
   public void execute(String userId, String technologyId, String recommendationsIds,
       String commentsIds, String serverUrl) {
-    if (commentsIds != null && commentsIds != "") {
+    if (!commentsIds.isEmpty()) {
       String[] commentIds = commentsIds.split(",");
       List<TechnologyComment> comments = new ArrayList<TechnologyComment>();
       for (String id : commentIds) {
         comments.add(technologyCommentDao.findById(Long.parseLong(id)));
       }
     }
-    if (recommendationsIds != null && recommendationsIds != "") {
+    if (!recommendationsIds.isEmpty()) {
       String[] recommendIds = recommendationsIds.split(",");
       List<TechnologyRecommendation> recommendations = new ArrayList<TechnologyRecommendation>();
       for (String id : recommendIds) {
@@ -125,25 +124,23 @@ public class EmailServiceImpl implements EmailService {
     }
     TechGalleryUser user = TechGalleryUserDAOImpl.getInstance().findById(Long.parseLong(userId));
     Technology technology = TechnologyDAOImpl.getInstance().findById(technologyId);
-    
-    //TODO extract method to build template (mustache).
+
+    // TODO extract method to build template (mustache).
     Map<String, String> variableValue = new HashMap<String, String>();
     variableValue.put("${receiverName}", user.getName());
     variableValue.put("${user}", user.getName());
     variableValue.put("${tecnology}", technology.getName());
-    EmailConfig email = new EmailConfig(
-        subject, "emailtemplates" + File.separator + template,
-        variableValue, null, reason,
-        user.getEmail());
+    EmailConfig email = new EmailConfig(subject, "emailtemplates" + File.separator + template,
+        variableValue, null, reason, user.getEmail());
     sendEmail(email);
   }
-  
+
   private void sendEmail(EmailConfig email) {
     try {
       Message msg = prepareMessage(email);
       Transport.send(msg);
       registerEmailNotification(email, true);
-      
+
     } catch (Throwable err) {
       Long notificationId = registerEmailNotification(email, false);
       log.log(Level.SEVERE, "Error when attempting to send email " + notificationId, err);
@@ -152,7 +149,7 @@ public class EmailServiceImpl implements EmailService {
 
   private Message prepareMessage(EmailConfig email)
       throws UnsupportedEncodingException, MessagingException {
-    
+
     Properties props = new Properties();
     Session session = Session.getDefaultInstance(props, null);
     Message msg = new MimeMessage(session);
@@ -166,23 +163,23 @@ public class EmailServiceImpl implements EmailService {
     return msg;
   }
 
-  private long registerEmailNotification(EmailConfig email, boolean success) {    
+  private long registerEmailNotification(EmailConfig email, boolean success) {
     EmailNotification emailNotification = new EmailNotification();
     emailNotification.setRecipients(Arrays.asList(email.getTo()));
     emailNotification.setRule(email.getRule());
     emailNotification.setReason(email.getReason());
     emailNotification.setTimestampSend(System.currentTimeMillis());
     emailNotification.setEmailStatus(success ? "SUCCESS" : "FAILURE");
-    
-    Key<EmailNotification> key = emailNotificationDao.add(emailNotification);    
+
+    Key<EmailNotification> key = emailNotificationDao.add(emailNotification);
     return key.getId();
   }
-  
+
   private InternetAddress getFrom() throws UnsupportedEncodingException {
-    if (from == null ) {
+    if (from == null) {
       String appId = ApiProxy.getCurrentEnvironment().getAppId();
       int tilde = appId.indexOf('~');
-      if (tilde >= 0) { //TODO make this into an environment property
+      if (tilde >= 0) { // TODO make this into an environment property
         appId = appId.substring(tilde + 1);
       }
       String addr = "no-reply@" + appId + ".appspotmail.com";
@@ -191,7 +188,7 @@ public class EmailServiceImpl implements EmailService {
     }
     return from;
   }
-  
+
   /**
    * Push one daily email to email queue for each follower user in each technology that have
    * followers.
@@ -205,9 +202,7 @@ public class EmailServiceImpl implements EmailService {
       for (TechnologyFollowers technologyFollowers : techFollowers) {
         Technology technology = technologyFollowers.getTechnology().get();
         if (technologyFollowers.getFollowers().size() > 0) {
-          Date date = new Date();
           Calendar cal = Calendar.getInstance();
-          cal.setTime(date);
           cal.add(Calendar.DAY_OF_MONTH, -1);
           Date dateYesterday = cal.getTime();
           String dailyRecommendationsIds =
@@ -215,7 +210,7 @@ public class EmailServiceImpl implements EmailService {
           String dailyCommentsIds =
               technologyCommentDao.findAllCommentsIdsStartingFrom(dateYesterday);
           List<Ref<TechGalleryUser>> followers = technologyFollowers.getFollowers();
-          if (!"".equals(dailyRecommendationsIds) || !"".equals(dailyCommentsIds)) {
+          if (!dailyRecommendationsIds.isEmpty() || !dailyCommentsIds.isEmpty()) {
             for (Ref<TechGalleryUser> ref : followers) {
               TechGalleryUser follower = ref.get();
               push(follower, technology, dailyRecommendationsIds, dailyCommentsIds);
