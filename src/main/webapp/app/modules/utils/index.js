89f7ba24eb19cbbec05f69146509efba1ca29ed2
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-10-27 10:21:02 -0200

    [TECG-272][TECG-319] Add loading as a directive

--- src/main/webapp/app/modules/utils/index.js
+++ src/main/webapp/app/modules/utils/index.js
@@ -1,5 +1,6 @@
 angular
   .module('TechGallery.Utils', [])
   .filter('nl2br', require('./filters/nl2brFilter'))
+  .directive('loading', require('./directives/LoadingDirective'));
 
 module.exports = 'TechGallery.Utils';
