fff88bba47158e5a9b05beec94b9cfb5c41efc7e
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-10-28 15:29:33 -0200

    [TECG-272][TECG-319]  Add application main navigation

--- src/main/webapp/app/modules/core/controllers/AppController.js
+++ src/main/webapp/app/modules/core/controllers/AppController.js
@@ -1,5 +1,16 @@
 module.exports = function ($window, $rootScope, AuthService) {
+
+  /**
+   * Object context
+   * @type {Object}
+   */
   var context = this;
+
+  /**
+   * Loading state
+   * @type {Boolean}
+   */
+  this._loading = true;
 
   /**
    * The app name
@@ -19,9 +30,35 @@ module.exports = function ($window, $rootScope, AuthService) {
    */
   $rootScope.isApiLoaded = false;
 
+  /**
+   * User login state
+   * @type {Boolean}
+   */
+  $rootScope.isUserLogged = true;
+
+  /**
+   * Ìnitialize AuthService if the API isn't loaded yet
+   * @return {Void}
+   */
   $window.gapiClientLoaded = function () {
     if (!$rootScope.isApiLoaded) {
       AuthService.init();
     }
+  }
+
+  /**
+   * Return loading state
+   * @return {Boolean}
+   */
+  this.isLoading = function () {
+    return context._loading;
+  }
+
+  /**
+   * Set loading state
+   * @param {Boolean} state The state to be set
+   */
+  this.setLoading = function (state) {
+    context._loading = !!state;
   }
 }
