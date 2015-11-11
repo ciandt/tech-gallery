eec5a6fb60c49395623bd102afe2dfed8fe94af8
Gabriel Izaias <gabriel.izaias@gmail.com>
2015-11-11 11:17:40 -0200

    [TECG-272][TECG-321] Tabs to Spaces & Docblock

--- src/main/webapp/app/modules/core/controllers/AppController.js
+++ src/main/webapp/app/modules/core/controllers/AppController.js
@@ -6,8 +6,16 @@ module.exports = function ($scope, $rootScope, AppService, AuthService, UserServ
    */
   var context = this;
 
+  /**
+   * Bind app.alert to the Service model
+   * @type {Object}
+   */
   this.alert = AppService.alert;
 
+  /**
+   * Bind the alert close method to the Service
+   * @type {Function}
+   */
   this.closeAlert = AppService.closeAlert;
 
   $rootScope.$watch('apiLoaded', function(newValue, oldValue) {
@@ -17,22 +25,22 @@ module.exports = function ($scope, $rootScope, AppService, AuthService, UserServ
   });
 
   function checkAuth(immediate){
-	   AuthService.checkAuth(afterLogin, immediate);
+     AuthService.checkAuth(afterLogin, immediate);
   }
 
   var afterLogin = function(data){
-  	if(data){
-  		$rootScope.isUserLogged = true;
+    if(data){
+      $rootScope.isUserLogged = true;
       UserService.getUserInformations();
-    	$scope.$apply();
-  	}else{
+      $scope.$apply();
+    }else{
       $rootScope.isUserLogged = false;
       $scope.$apply();
     }
   }
 
   this.login = function(){
-  	checkAuth(false);
+    checkAuth(false);
   }
 
 }
