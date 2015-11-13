6dc9cc89115896e88c48200bea70f28540f84773
Thulio Ribeiro <tsr_thulio@hotmail.com>
2015-11-13 17:52:44 -0200

    [TECG-272] Adjust front-end slugify

--- src/main/webapp/app/modules/technology/services/TechnologyService.js
+++ src/main/webapp/app/modules/technology/services/TechnologyService.js
@@ -101,7 +101,7 @@ module.exports = function($q, $timeout, $rootScope) {
 
   function slugify(text){
     return text.toString().toLowerCase()
-        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/\s+/g, '_')           // Replace spaces with -
         .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
         .replace(/\-\-+/g, '-')         // Replace multiple - with single -
         .replace(/^-+/, '')             // Trim - from start of text
