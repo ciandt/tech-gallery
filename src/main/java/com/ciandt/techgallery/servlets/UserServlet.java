31bfa1716e940dec86ad1517fa8e7a646b0c4bd4
Sidharta Noleto <sidvelloso@gmail.com>
2016-04-27 14:56:37 -0300

    Revert "Ajustes no servlet de Exportacao para validar o perfil internamente. (#220)"
    This reverts commit 086abcbaebe203e380612a594a41ce95fe594c9c.

--- src/main/java/com/ciandt/techgallery/servlets/UserServlet.java
+++ src/main/java/com/ciandt/techgallery/servlets/UserServlet.java
@@ -3,8 +3,6 @@ package com.ciandt.techgallery.servlets;
 import static com.ciandt.techgallery.utils.ExportUtils.createCsvUsersProfile;
 import static com.ciandt.techgallery.utils.ExportUtils.createXlsUsersProfile;
 
-import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,14 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.ciandt.techgallery.persistence.model.TechGalleryUser;
 import com.ciandt.techgallery.persistence.model.profile.UserProfile;
-import com.ciandt.techgallery.service.UserServiceTG;
-import com.ciandt.techgallery.service.impl.UserServiceTGImpl;
 import com.ciandt.techgallery.service.impl.profile.UserProfileServiceImpl;
 import com.ciandt.techgallery.service.profile.UserProfileService;
-import com.google.api.server.spi.response.ForbiddenException;
-import com.google.api.server.spi.response.NotFoundException;
 
 @SuppressWarnings("serial")
 public class UserServlet extends HttpServlet {
@@ -30,27 +23,13 @@ public class UserServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) {
 
-    String userName = request.getUserPrincipal().getName();
-    UserServiceTG uService = UserServiceTGImpl.getInstance();
-
     String exportType = request.getParameter("type");
     response.setCharacterEncoding("UTF-8");
 
-    try {
-      TechGalleryUser user = uService.getUserByEmail(userName);
-
-      if(!user.isAdmin()) {
-        throw new ForbiddenException("Você precisa ser um administrador do TechGallery para esta operação.");
-      }
-
-      if (exportType != null && exportType.equals("xls")) {
-        exportXls(response);
-      } else {
-        exportCsv(response);
-      }
-
-    } catch (Exception e) {
-      _LOG.log(Level.SEVERE, "Export exception: ", e);
+    if (exportType != null && exportType.equals("xls")) {
+      exportXls(response);
+    } else {
+      exportCsv(response);
     }
   }
 
@@ -59,27 +38,35 @@ public class UserServlet extends HttpServlet {
     doGet(request, response);
   }
 
-  private void exportCsv(HttpServletResponse response) throws NotFoundException, IOException {
+  private void exportCsv(HttpServletResponse response) {
 
     UserProfileService service = UserProfileServiceImpl.getInstance();
     response.setContentType("text/csv");
     response.setHeader("Content-Disposition", "attachment; filename=\"Colaboradores.csv\"");
 
-    List<UserProfile> allUsersProfile = service.findAllUsersProfile();
-    if (allUsersProfile != null) {
-      response.getOutputStream().write(createCsvUsersProfile(allUsersProfile));
+    try {
+      List<UserProfile> allUsersProfile = service.findAllUsersProfile();
+      if (allUsersProfile != null) {
+        response.getOutputStream().write(createCsvUsersProfile(allUsersProfile));
+      }
+    } catch (Exception e) {
+      _LOG.log(Level.SEVERE, "Export exception: ", e);
     }
   }
 
-  private void exportXls(HttpServletResponse response) throws NotFoundException, IOException {
+  private void exportXls(HttpServletResponse response) {
 
     UserProfileService service = UserProfileServiceImpl.getInstance();
     response.setContentType("application/ms-excel");
     response.setHeader("Content-Disposition", "attachment; filename=\"Colaboradores.xls\"");
 
-    List<UserProfile> allUsersProfile = service.findAllUsersProfile();
-    if (allUsersProfile != null) {
-      response.getOutputStream().write(createXlsUsersProfile(allUsersProfile));
+    try {
+      List<UserProfile> allUsersProfile = service.findAllUsersProfile();
+      if (allUsersProfile != null) {
+        response.getOutputStream().write(createXlsUsersProfile(allUsersProfile));
+      }
+    } catch (Exception e) {
+      _LOG.log(Level.SEVERE, "Export exception: ", e);
     }
   }
 }
