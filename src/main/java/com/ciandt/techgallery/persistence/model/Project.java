f79efd5e1ef1eec7492697837ac8090b1d59e905
Moizes Papa <moizes@ciandt.com>
2018-08-02 17:25:29 -0300

    KOGO-8375 Tech Gallery Changes - Specific Knowledge Mapping KOGO-8481 Include 'project specific' field in technology addEdit screen

---
+++ src/main/java/com/ciandt/techgallery/persistence/model/Project.java
@@ -0,0 +1,54 @@
+package com.ciandt.techgallery.persistence.model;
+
+import com.ciandt.techgallery.service.transformer.ProjectTransformer;
+import com.google.api.server.spi.config.ApiTransformer;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+/**
+ * Project entity.
+ *
+ * @author moizes
+ *
+ */
+@Entity
+@ApiTransformer(ProjectTransformer.class)
+public class Project extends BaseEntity<String> {
+
+    /*
+     * Constants --------------------------------------------
+     */
+    public static final String ID = "id";
+    public static final String NAME = "name";
+
+    /*
+     * Attributes --------------------------------------------
+     */
+    @Id
+    private String id;
+
+    @Index
+    private String name;
+
+    /*
+     * Getter's and Setter's --------------------------------------------
+     */
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
