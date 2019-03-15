f66513195b5d163d9341ad246e84f6c65cfcefa4
mpellini <mpellini@ciandt.com>
2019-03-15 15:08:02 -0300

    Update ProjectEndpoint.java
    Fixing throws section

--- src/main/java/com/ciandt/techgallery/service/endpoint/ProjectEndpoint.java
+++ src/main/java/com/ciandt/techgallery/service/endpoint/ProjectEndpoint.java
@@ -72,7 +72,7 @@ public class ProjectEndpoint {
      * @throws BadRequestException when some request parameter is wrong missing
      */
     @ApiMethod(name = "getAllProjects", path = "projects", httpMethod = "get")
-    public List<Project> getProjects(User user) throws throws InternalServerErrorException,
+    public List<Project> getProjects(User user) throws InternalServerErrorException,
             BadRequestException, OAuthRequestException, NotFoundException{
         return service.getProjects();
     }
